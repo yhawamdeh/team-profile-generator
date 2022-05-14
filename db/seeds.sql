9ca23c1ac600c4398d46976e3f1a036e6696c34f
yhawamdeh <yalhawamdeh@gmail.com>
2022-05-14 18:31:02 -0400

    updates

---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+INSERT INTO
+    department (name)
+VALUES
+    ('sales'),
+    ('hr'),
+    ('marketing'),
+    ('it');
+
+INSERT INTO
+    role (title, salary, department_id)
+VALUES
+    ('sales representative', 50000.00, 1),
+    ('call center', 40000.00, 2),
+    ('call center', 40000.00, 2),
+    ('lead sales', 60000.00, 1),
+    ('social media posts', 40000.00, 3),
+    ('junior develepor', 80000.00, 4),
+    ('senior developer', 110000.00, 4);
