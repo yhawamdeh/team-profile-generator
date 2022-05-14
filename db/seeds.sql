21336e923a7bfabc1f9f079bdc1ee43a33091cef
yhawamdeh <yalhawamdeh@gmail.com>
2022-05-14 19:03:40 -0400

    updates

--- db/seeds.sql
+++ db/seeds.sql
@@ -16,3 +16,15 @@ VALUES
     ('social media posts', 40000.00, 3),
     ('junior develepor', 80000.00, 4),
     ('senior developer', 110000.00, 4);
+
+INSERT INTO 
+    voters (first_name, last_name, role_id, manager_id)
+VALUES
+    ('James', 'Dento', 1, NULL),
+    ('Nikola', 'Fraser', 2, NULL),
+    ('Amir', 'Hussein', 3, NULL),
+    ('Sing', 'Li', 4, NULL),
+    ('Kumar', 'Khan', 5, NULL),
+    ('Famouke', 'Doumbia', 6, NULL),
+    ('Mohamed', 'Omar', 7, NULL);    
+    
